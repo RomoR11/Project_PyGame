ff4de00f1d6a97c7699884750282c92c584bc40a
Adamyauskas <pbabin558@yandex.ru>
2023-12-24 15:08:56 +0300

    Заставка и пока всё

--- main.py
+++ main.py
@@ -1,4 +1,56 @@
 import pygame
+import sys
 
 
 pygame.init()
+
+
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+FPS = 50
+clock = pygame.time.Clock()
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ['Чтобы начать игру нажмите любую кнопку']
+
+    screen.fill((255, 255, 255))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    running = True
+    start_screen()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                all_sprites.update(event, level)
+        screen.fill('black')
+        pygame.display.flip()
+    pygame.quit()
