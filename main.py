d72b25c1f1b73b26e89b2fef91749ae665a779b3
TopPC <rm.mikhanov@gmail.com>
2023-12-31 17:13:07 +0300

    New generate level and camera!!!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import os
+import csv
 
 
 pygame.init()
@@ -55,80 +56,108 @@ def load_image(name, colorkey=None):
     return image
 
 
-def load_level(filename):
+def generate_level(filename):
     filename = "data/" + filename
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-    max_width = max(map(len, level_map))
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+    with open(filename, 'r') as csvfile:
+        r = csv.reader(csvfile, delimiter=';', quotechar='"')
+        data = [[row[0], row[1], row[2]] for row in r]
+    new_player, max_width, max_height = None, 0, 0
+    for i in data:
+        if i[0] == 'player':
+            new_player = Player(int(i[1]), int(i[2]))
+        elif i[0] == 'platform':
+            Platform('platform', int(i[1]), int(i[2]))
+        elif i[0] == 'level':
+            max_width, max_height = int(i[1]), int(i[2])
+    return new_player, max_width, max_height
 
 
-platform_image = {'platform': pygame.transform.scale(load_image('platform_py.png'), (80, 80)),
-                  'empty': pygame.transform.scale(load_image('empty.png'), (80, 80))}
+platform_image = {'platform': load_image('platform_py.png')}
 player_image = pygame.transform.scale(load_image('hero.png'), (80, 80))
-pl_height = pl_width = 80
 
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self, platform_type, pos_x, pos_y):
         super().__init__(platform_group, all_sprites)
         self.image = platform_image[platform_type]
-        self.rect = self.image.get_rect().move(
-            pl_width * pos_x, pl_height * pos_y)
+        self.rect = self.image.get_rect().move(pos_x, pos_y)
+        self.mask = pygame.mask.from_surface(self.image)
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
-        self.rect = self.image.get_rect().move(
-            pl_width * pos_x + 15, pl_height * pos_y + 5)
         self.x = pos_x
+        self.x0 = pos_x
         self.y = pos_y
+        self.rect = self.image.get_rect().move(self.x, self.y)
+        self.mask = pygame.mask.from_surface(self.image)
         self.flip = False
 
-    def update(self, *args, **kwargs):
+    def twist(self, *args):
         if args:
-            event, level = args[0], args[1]
-            if event.key == pygame.K_LEFT and self.x - 1 >= 0:
-                self.x -= 1
+            event = args[0]
+            if event.key == pygame.K_LEFT:
+                self.x0 -= 1
                 if not self.flip:
                     self.image = pygame.transform.flip(self.image, True, False)
                     self.flip = True
-            elif event.key == pygame.K_RIGHT and len(level[self.y]) > self.x + 1:
-                self.x += 1
+            elif event.key == pygame.K_RIGHT:
+                self.x0 += 1
                 if self.flip:
                     self.image = pygame.transform.flip(self.image, True, False)
                     self.flip = False
-            self.rect = self.image.get_rect().move(pl_width * self.x + 15, pl_height * self.y + 5)
+
+    def update(self, *args, **kwargs):
+        if args:
+            event, w, h = args[0], args[1], args[2]
+            if event.key == pygame.K_LEFT and self.x - 1 >= 0:
+                self.x -= 1
+                self.x0 -= 1
+                self.twist(event)
+            elif event.key == pygame.K_RIGHT and self.x + 1 <= level_x:
+                self.x += 1
+                self.x0 += 1
+                self.twist(event)
+            self.rect = self.image.get_rect().move(self.x, self.y)
 
 
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Platform('empty', x, y)
-            elif level[y][x] == '#':
-                Platform('platform', x, y)
-            elif level[y][x] == '$':
-                Platform('empty', x, y)
-                new_player = Player(x, y)
-    return new_player, x, y, level
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+        self.event = None
+
+    def apply(self, obj):
+        if player.x0 <= 100 and self.event:
+            player.update(self.event, level_x, level_y)
+        else:
+            obj.rect.x -= self.dx
+
+    def update(self, *args):
+        if args:
+            self.event = args[0]
+            self.dx = -1 if self.event.key == pygame.K_LEFT else 1
+            player.twist(event)
 
 
-player, level_x, level_y, level = generate_level(load_level('level.txt'))
+player, level_x, level_y = generate_level('level.csv')
 
 if __name__ == '__main__':
     running = True
     start_screen()
+    camera = Camera()
+    camera.update()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    all_sprites.update(event, level)
+                    camera.update(event)
+                    for sprite in platform_group:
+                        camera.apply(sprite)
         screen.fill('black')
         all_sprites.draw(screen)
         player_group.draw(screen)
